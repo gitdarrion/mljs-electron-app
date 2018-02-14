a957012eae369df8d7027c34f660dfd2c18c2159
Darrion Dion Banks <darrion@darrion-mbp.local>
2018-02-13 22:14:21 -0500

    Added 'Open' button to load files.

--- renderer.js
+++ renderer.js
@@ -1,8 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const dialog = require('electron').dialog;
+const { dialog } = require('electron').remote;
+console.log(dialog);
 
 function showOpenDialog() {
-    dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] })
+    dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] });
 }
+
+document.querySelector('#btnOpenFile').addEventListener('click', showOpenDialog);
