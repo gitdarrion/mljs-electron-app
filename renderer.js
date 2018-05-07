0d4cdc4d0c86ef3fe5f3af38845fba9e9c825083
Darrion Dion Banks <darrion@darrion-mbp.local>
2018-05-07 14:29:57 -0400

    Open CSV file feature is functional.

--- renderer.js
+++ renderer.js
@@ -1,24 +1,58 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var dialog = require('electron').dialog;
+
+// MODULES
+
+var remote = require('electron').remote;
+var dialog = remote.dialog; 
 var fs = require('fs');
 var csv = require('fast-csv');
-var csv_file_name = "";
-var csv_data = [];
 
+// ELEMENT FUNCTIONALITY
 
-function loadCsvFile() {
+function onClickButtonOpenFile() {
+
+    // TODO: Limit the number of files 1. 
     
-    csv_file_name = dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'], filters: [{ name: "CSV", extensions: ['csv'] }] })[0];
+    var csv_files = dialog.showOpenDialog({ 
+
+        properties: ['openFile', 'openDirectory', 'multiSelections'], 
+        filters: [{ name: "CSV", extensions: ['csv'] }] 
+
+    }); // List of file names.
     
-    var stream = fs.createReadStream(String(csv_file_name));
+    var csv_file = csv_files[0]; // If multiple files selected, get name of first one.
+    var csv_data = []; // Multi-dimensional array of rows and data read from file.
+    var csv_row = 0; // Index of current row // Incremented after each row is read.
+    var stream = fs.createReadStream(String(csv_file)); // Node.js generic pipeline for data from a file.
 
     csv.fromStream(stream, { headers: true })
         .on("data", function (data) {
-            console.log(data);
+
+            headers = Object.keys(data); 
+            number_of_headers = Object.keys(data).length; 
+            csv_data.push([]);
+
+            for (var csv_col = 0; csv_col < number_of_headers; csv_col++) {
+                csv_data[csv_row][csv_col] = data[headers[csv_col]]; 
+            }
+
+            csv_row += 1; 
+
         })
         .on("end", function () {
-            console.log("Data loaded."); 
-        }); 
+
+            console.log("Read ", csv_data.length, " lines from ", csv_file); 
+            console.log("End."); 
+
+        }); // The 'fast-csv' module pipeline that is engineered for processing CSV file data.
 }
+
+// ELEMENTS
+
+var button_open_file = document.querySelector("#button_open_file");
+
+// EVENTS
+
+button_open_file.addEventListener("click", onClickButtonOpenFile);
